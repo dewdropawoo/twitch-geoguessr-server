aad54e64841cf0c1aed8fcbc01e8afe6a5a4410a
yeah568 <awoo@dewdrop.dog>
2021-03-21 14:36:01 -0700

    more stuff

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jwt');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+});
+
+app.options('*', cors());
+
+// Listen to the App Engine-specified port, or 8000 otherwise
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+const io = require('socket.io')(server);
+
+
+const rooms = {};
+
+
+
+app.post('/submit', cors(), (req, res) => {
+    // TODO: verify jwt
+    console.log(req.body.channel);
+    console.log(req.body.userId);
+    console.log(req.body.latLng);
+
+    const { channel, userId, latLng } = req.body;
+
+    const room = rooms[channel];
+    if (!room) {
+        return res.status(404).send({
+            message: 'Streamer not active',
+        });
+    }
+
+    if (room.voters.has(userId)) {
+        return res.status(400).send({
+            message: 'Already voted this round',
+        });
+    }
+
+    
+    room.voters.add(userId);
+
+    const cartesian = latLngToCartesian(latLng);
+    room.averageCartesian = [
+        room.averageCartesian[0] + cartesian[0],
+        room.averageCartesian[1] + cartesian[1],
+        room.averageCartesian[2] + cartesian[2],
+    ];
+
+    room.socket.emit('vote', {count: room.voters.size(), average: cartesianToLatLng(room.averageCartesian)});
+});
+
+
+
+io.on('connection', socket => {
+  socket.on('live', (data) => {
+    // TODO: verify jwt
+    const channel = data.channel;
+    socket.channelId = channel;
+    rooms[channel] = {
+        voters: new Set(),
+        averageCartesian = [0, 0, 0],
+        socket,
+    };
+  });
+
+  socket.on('start round', () => {
+
+  });
+
+  socket.on('stop round', () => {
+    const room = rooms[channel];
+  });
+
+  socket.on('disconnect', () => {
+    delete rooms[socket.channelId];
+  });
+});
+
+
+
+
+
+
+
+const RAD2DEG = 180 / Math.PI;
+const DEG2RAD = Math.PI / 180;
+
+// https://gis.stackexchange.com/a/7566
+function calculateAverageLatLng(latLngs) {
+    const sumCartesian = latLngs.reduce(([xAcc, yAcc, zAcc], latLng) => {
+        const [x, y, z] = latLngToCartesian(latLng);
+        return [xAcc + x, yAcc + y, zAcc + z];
+    }, [0, 0, 0]);
+
+    return cartesianToLatLng(sumCartesian);
+}
+
+function latLngToCartesian([lat, lng]) {
+    const latRad = DEG2RAD * lat;
+    const lngRad = DEG2RAD * lng;
+    const sinLat = Math.sin(latRad);
+    return [
+        sinLat * Math.cos(lngRad),
+        sinLat * Math.sin(lngRad),
+        Math.cos(latRad),
+    ];
+}
+
+function cartesianToLatLng([x, y, z]) {
+    const lat = RAD2DEG * Math.atan2(z, Math.sqrt(x * x + y * y));
+    const lng = RAD2DEG * Math.atan2(-y, x);
+    return [lat, lng];
+}
